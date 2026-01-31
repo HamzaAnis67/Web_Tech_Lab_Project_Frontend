25731854a0297ee796ddc0a6c89253f20c80b1ac
darkdevil0012 <Smarthamza123@outlook.com>
2026-01-31 16:03:41 +0500

    fixed code

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# 🛒 Forever 
+# 🛒 Forever
 
 Welcome to **Forever **, an eCommerce platform offering a wide selection of products with convenient filtering options. This website is designed for a smooth and enjoyable shopping experience, with modern web technologies powering the interface.
 
@@ -41,29 +41,21 @@ You can visit the live website here: [Forever ](https://forever-ecom.netlify.app
 - **State Management**:
   - React Hooks and Context API for managing global state.
 
-
-
-- **Deployment**:
-  - [Netlify](https://www.netlify.com/) for fast and reliable hosting.
-
 ## 🚀 Getting Started
 
 1. Clone the repository:
-    ```bash
-    git clone https://github.com/faisal-din/forever-eCommerce-website-
-    ```
+   ```bash
+   git clone https://github.com/HamzaAnis67/Web_Tech_Lab_Project_Frontend
+   ```
 2. Navigate to the project directory:
-    ```bash
-    cd forever-ecom
-    ```
+   ```bash
+   cd forever-ecom
+   ```
 3. Install dependencies:
-    ```bash
-    npm install
-    ```
+   ```bash
+   npm install
+   ```
 4. Run the app:
-    ```bash
-    npm start
-    ```
-
-
-
+   ```bash
+   npm start
+   ```
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/Home";
+import Home from "./pages/Home.jsx";
 import Collection from "./pages/Collection";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -16,28 +16,28 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-	return (
-		<div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
-			<ToastContainer />
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <ToastContainer />
 
-			<Navbar />
-			<SearchBar />
+      <Navbar />
+      <SearchBar />
 
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/collection" element={<Collection />} />
-				<Route path="/about" element={<About />} />
-				<Route path="/contact" element={<Contact />} />
-				<Route path="/product/:productId" element={<Product />} />
-				<Route path="/cart" element={<Cart />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/place-order" element={<PlaceOrder />} />
-				<Route path="/orders" element={<Orders />} />
-			</Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/orders" element={<Orders />} />
+      </Routes>
 
-			<Footer />
-		</div>
-	);
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
